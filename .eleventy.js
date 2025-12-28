8ce7226ba958b34f7c1bc2695721d706a517cac9
Hamlet6753 <andrewhomenko675@gmail.com>
2025-12-28 16:28:02 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+
+  // ROOT static files
+  eleventyConfig.addPassthroughCopy({ "src/sitemap.xml": "sitemap.xml" });
+  eleventyConfig.addPassthroughCopy({ "src/robots.txt": "robots.txt" });
+  eleventyConfig.addPassthroughCopy({ "src/site.webmanifest": "site.webmanifest" });
+
   eleventyConfig.addCollection("blog", (collectionApi) => {
     return collectionApi.getFilteredByGlob("src/posts/*.md");
   });
